a7ec304485678edb109ebae1a7e2763b497c2bd7
Alex Sivik <alexander.sivik@gmail.com>
2020-11-15 20:52:58 +0200

    add api github

--- modules/cabinet/controllers/DefaultController.php
+++ modules/cabinet/controllers/DefaultController.php
@@ -16,10 +16,15 @@ class DefaultController extends Controller
     public function actionIndex()
     {
 		$client = new \Github\Client();
-		$activity = $client->api('sivik1986')->starring()->configure('star')->all();
-$repos = $client->api('alex-sivik1986')->repositories();
 
-var_dump($activity); die;
+		$token = '3851253c80a907f50bca5fcaf8f4d93856e890f4'; # https://help.github.com/en/articles/creating-a-personal-access-token-for-the-command-line
+		$client = new \Github\Client();
+		$client->authenticate($token,null,\Github\Client::AUTH_HTTP_TOKEN);
+
+		$file = $client->api('user')->repositories('anotherjesse', $type = 'owner', $sort = 'full_name', $direction = 'asc', $visibility = 'all', $affiliation = 'owner,collaborator,organization_member');
+
+		var_dump($file);
+ die;
 
         return $this->render('index');
     }
