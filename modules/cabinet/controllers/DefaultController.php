219e5767e2a0345b5890f51542b42e17424fd075
Alex Sivik <alexander.sivik@gmail.com>
2020-11-16 22:13:41 +0200

    view grid 50 repositories

--- modules/cabinet/controllers/DefaultController.php
+++ modules/cabinet/controllers/DefaultController.php
@@ -3,7 +3,7 @@
 namespace app\modules\cabinet\controllers;
 
 use yii\web\Controller;
-
+use yii\data\ArrayDataProvider;
 /**
  * Default controller for the `cabinet` module
  */
@@ -16,16 +16,20 @@ class DefaultController extends Controller
     public function actionIndex()
     {
 		$client = new \Github\Client();
+		$file = $client->api('repo')->all();
+        $provider = new ArrayDataProvider([
+							'allModels' => $file,
+							'pagination' => [
+								'pageSize' => 50,
+							],
+							'sort' => [
+								'attributes' => ['id', 'name'],
+							],
+						]);
 
-		$token = '3851253c80a907f50bca5fcaf8f4d93856e890f4'; # https://help.github.com/en/articles/creating-a-personal-access-token-for-the-command-line
-		$client = new \Github\Client();
-		$client->authenticate($token,null,\Github\Client::AUTH_HTTP_TOKEN);
-
-		$file = $client->api('user')->repositories('anotherjesse', $type = 'owner', $sort = 'full_name', $direction = 'asc', $visibility = 'all', $affiliation = 'owner,collaborator,organization_member');
-
-		var_dump($file);
- die;
-
-        return $this->render('index');
+        return $this->render('index', 
+			[
+				'repositories' => $provider
+			]);
     }
 }
